ca7dc89472e3345fc427dac75d03f4de1b6251d6
jeevanraj73 <108092517+jeevanraj73@users.noreply.github.com>
2024-02-25 17:57:58 +0530

    Create connection.java

---
+++ connection.java
@@ -0,0 +1,30 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+// Global connection Class
+public class connection {
+	static Connection con; // Global Connection Object
+	public static Connection getConnection()
+	{
+		try {
+			
+			
+			String mysqlJDBCDriver
+				= "com.mysql.cj.jdbc.Driver"; //jdbc driver
+			System.out.println("1");
+			String url
+				= "jdbc:mysql://localhost:3306/food"; //mysql url
+			String user = "root";	 //mysql username
+			String pass = "Medha@2020"; //mysql passcode
+			Class.forName(mysqlJDBCDriver);
+			con = DriverManager.getConnection(url, user,
+											pass);
+		}
+		catch (Exception e) {
+			System.out.println("Connection Failed!");
+		}
+
+		return con;
+	}
+}
